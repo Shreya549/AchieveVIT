0621811b9b49f879ec2e5b2d7bf726b466203b03
Shreya549 <shreya.chatterjee2018@vitstudent.ac.in>
2020-09-22 22:08:49 +0530

    Procfile

--- AchieveVIT/settings.py
+++ AchieveVIT/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -149,3 +150,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'static')
 ]
+
+django_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn achievevit.wsgi
