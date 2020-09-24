1683c1b8a590f6a85e8a776316d12031c08487c8
Shreya549 <shreya.chatterjee2018@vitstudent.ac.in>
2020-09-24 18:32:56 +0530

    views created

--- Profile/views.py
+++ Profile/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions, generics
+from rest_framework. response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .serializers import FacultyProfileSerializer, HRProfileSerializer
+from .models import FacultyProfile, HRProfile
 
 # Create your views here.
+class FacultyProfileView(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = FacultyProfileSerializer
+
+    def get_queryset(self):
+        profile = FacultyProfile.objects.get(owner = self.request.user)
+        return profile
+
+    def perform_create(self, serializer):
+        serializer.save(owner = self.request.user)
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
+
+
+class HRProfileView(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = HRProfileSerializer
+
+    def get_queryset(self):
+        profile = HRProfile.objects.get(owner = self.request.user)
+        return profile
+
+    def perform_create(self, serializer):
+        serializer.save(owner = self.request.user)
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
+
+
+        
